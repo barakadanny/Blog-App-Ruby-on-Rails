5396a48d1b450a1f1425311a6131f9e5e815fb87
Danny <danny@Dannys-MacBook-Pro.local>
2022-12-07 12:02:35 +0300

    Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
-class Like < ApplicationRecord
+class Like < ApplicationRecord::Base
+  belongs_to :user
+  belongs_to :post
+
+  def update_likes_counter
+    post.increment!(:likes_count)
+  end
 end
