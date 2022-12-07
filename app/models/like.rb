8527711a6bf0b435a7dc76969dd235351ca59657
Danny <danny@Dannys-MacBook-Pro.local>
2022-12-07 15:36:38 +0300

    Update methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
-class Like < ApplicationRecord::Base
+class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord::Base
+class User < ApplicationRecord
   has_many :posts
   has_many :comments, through: :posts
   has_many :likes, through: :posts
