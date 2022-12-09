4b07b1397c1e9702aeb9f4bdbf67518112ca406e
Danny <danny@Dannys-MacBook-Pro.local>
2022-12-09 11:55:13 +0300

    Comment test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  p person = User.create(
+    name: 'Duane',
+    photo: 'https://duane.com/me.png',
+    bio: 'Iam Duane David.',
+    post_counter: 0
+  )
+
+  post = Post.create(
+    title: 'Today',
+    text: 'Blue sky',
+    comments_counter: 0,
+    likes_counter: 0,
+    user_id: person.id
+  )
+
+  comment = Comment.create(post: post, user: person)
+
+  context 'update_comments_counter' do
+    comment.update_comments_counter
+
+    it ' incriment comments_counter' do
+      expect(Post.find(post.id).comments_counter).eql?(post.comments_counter + 1)
+    end
+  end
 end
