0458345fb2c87b3fc421799152ad46b77f178959
Danny <danny@Dannys-MacBook-Pro.local>
2022-12-13 14:41:31 +0300

    Linters

--- Gemfile
+++ Gemfile
@@ -77,4 +77,4 @@ end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-gem "tailwindcss-rails", "~> 2.0"
+gem 'tailwindcss-rails', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'posts/index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
