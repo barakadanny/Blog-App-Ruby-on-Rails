6312a60f9b6b8e0bb87a0589cf8c5ecf182baa9a
Danny <danny@Dannys-MacBook-Pro.local>
2022-12-17 14:44:16 +0300

    Update post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,32 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new
+    render :new, locals: {post: @post}
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    respond_to do |format|
+      format.hrml do
+        if @post.save
+          flash[:success] = 'Post created'
+          redirect_to user_posts_path(@user)
+        else
+          flash[:error] = 'Post not created'
+          render :new, locals: { post: @post }
+        end
+      end
+    end
+  end
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
 end
