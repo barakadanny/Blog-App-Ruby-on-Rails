009408615008175bd5aefaad1eb58ebc49cc06d2
Danny <barakadan421@gmail.com>
2022-12-20 21:40:05 +0300

    Update posts show test

--- spec/views/posts/post_show_spec.rb
+++ spec/views/posts/post_show_spec.rb
@@ -1,45 +1,42 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'renders users Post Page', type: :feature do
-#   before :each do
-#     @user = User.create(name: 'Baraka Danny', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-#                         bio: 'This is for testing purpose', post_counter: 6)
-#     @user2 = User.create(name: 'Danny John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-#                          bio: 'This is for testing purpose', post_counter: 9)
-#     @post1 = Post.create(user: @user, title: 'lion', text: 'anything on unit tests')
-#     @post2 = Post.create(user: @user, title: 'gun', text: 'anything on unit tests')
-#     Like.create(user: @user2, post: @post1)
-#     Like.create(user: @user2, post: @post2)
-#     Comment.create(user: @user2, post: @post1, text: 'This is a comment')
-#   end
+RSpec.describe 'Renders the post show page', type: :feature do
+  before :each do
+    @user = User.create(name: 'Baraka Danny', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                        bio: 'Am a ruby developer', post_counter: 9)
+    @first_post = Post.create(title: 'Hello', text: 'This is a post', comments_counter: 0, likes_counter: 0,
+                              user_id: @user.id)
+    5.times do |_i|
+      Comment.create(text: 'Interesting topic', user_id: @user.id, post_id: @first_post.id)
+    end
+    visit user_post_path(@first_post.user, @first_post)
+  end
 
-#   scenario 'It should show the post title' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.title)
-#   end
+  scenario 'displays the post title' do
+    expect(page).to have_content(@first_post.title)
+  end
 
-#   scenario 'It should show the post text' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.text)
-#   end
+  scenario 'disolays number of comments' do
+    expect(page).to have_content('5')
+  end
 
-#   scenario 'It should show the post likes' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.likes.count)
-#   end
+  scenario 'displays number of likes' do
+    expect(page).to have_content('0')
+  end
 
-#   scenario 'It should show the post comments' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.comments.count)
-#   end
+  scenario 'displays the content of the post' do
+    expect(page).to have_content(@first_post.text)
+  end
 
-#   scenario 'It should show the post author' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.user.name)
-#   end
+  scenario 'displays comments' do
+    expect(page).to have_content('Hello')
+  end
 
-#   scenario 'It should show the username of the commentor' do
-#     visit post_path(id: @post1.id)
-#     expect(page).to have_content(@post1.comments.first.user.name)
-#   end
-# end
+  scenario 'displays can see the username of each commentor' do
+    expect(page).to have_content(@user.name)
+  end
+
+  scenario 'I can see the comment each commentor left.' do
+    expect(page).to have_content('Interesting topic')
+  end
+end
